ac22d0dba0015e9eb0ecff22d0ad3b5273ff4a33
bipan82gill <56560207+bipan82gill@users.noreply.github.com>
2020-02-04 21:57:55 -0500

    Eat-da-burger

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name,devoured) VALUES("Cheese Burger", true);
+INSERT INTO burgers(burger_name) VALUES("Chilli Burger");
+INSERT INTO burgers(burger_name,devoured) VALUES("Veggie Burger", true);
+INSERT INTO burgers(burger_name,devoured) VALUES("Chicken Burger", true);
+
